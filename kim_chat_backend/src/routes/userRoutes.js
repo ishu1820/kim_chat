19d836cbdf36556200a60404a6ac8393eef8a17a
frustateduser <koustubhbadshah07@outlook.com>
2025-04-28 22:06:28 +0530

    added rate limit to userRoute

--- kim_chat_backend/src/routes/userRoutes.js
+++ kim_chat_backend/src/routes/userRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const {authMiddleware} = require("../middleware/authMiddleware");
+const rateLimit = require("express-rate-limit");
 
 
 
 const router = express.Router();
 
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests per windowMs
+  message: "Too many signup attempts from this IP, please try again later.",
+});
+
+
 // Protected route example
-router.get("/profile", authMiddleware, (req, res) => {
+router.get("/profile",limiter, authMiddleware, (req, res) => {
   res.json({ message: "Welcome to your profile", userId: req.user.id });
 });
 
